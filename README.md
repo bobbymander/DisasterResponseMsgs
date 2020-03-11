02df355705ecaed510ac4b43f185a3b0ee5908eb
bobbymander <bobby.mander@att.com>
2020-03-11 11:57:06 -0400

    Initial commit for disaster response project

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# DisasterResponseMsgs
+# Disaster Response Pipeline Project
+
+The purpose of this project is to analyze text messages from people who are involved with a disaster.
+
+Based on this NLP text analysis, we will determine what labels to apply to the message so that 
+emergency personnel can act on them as needed.  We will use NLP techniques along with machine learning
+classification using pipelines and grid search to accomplish this.
+
+## Data
+The data for this project can be found below.  There are 2 datasets, one for dogs and one for human faces.
+
+Messages:  
+Categories:  
+
+## Setup
+1. Run the following commands in the project's root directory to set up your database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+2. Run the following command in the app's directory to run your web app.
+    `python run.py`
+
+3. Go to https://view6914b2f4-3001.udacity-student-workspaces.com/
+
+## Libraries
+
+A listing of libraries installed and used by the notebook is below:
+
+1.  numpy:  computation
+2.  pandas:  data manipulation
+3.  sqlalchemy:  storing and loading data into an SQL DB file
+4.  nltk:  NLP tokenizing libraries
+5.  sklearn:  machine learning classification libraries
+6.  pickle:  model storing/loading libraries
---
+++ train_classifier.py
@@ -0,0 +1,93 @@
+import sys
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import f1_score
+import re
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+nltk.download(['punkt', 'wordnet'])
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('cleanmsgs', engine)
+    X = df['message']
+    Y = df.iloc[:, 3:]
+    return X, Y, list(Y)
+
+def tokenize(text):
+    text = text.lower() 
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model():
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer()),
+            ('moc', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    
+    return pipeline
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    
+    colnum = 0
+    for col in category_names:
+        report = classification_report(Y_test[[col]], y_pred[:, colnum])
+        colnum += 1
+        print(col, report)
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
